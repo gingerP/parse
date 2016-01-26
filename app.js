ffce3cd96e4b21ace3518cc618ce523e89098127
Kanstantsin Shydla <Kanstantsin_Shydla@epam.com>
2016-01-26 18:06:40 +0300

    WebSocket server.

--- app.js
+++ app.js
@@ -39,21 +39,15 @@ app.use(function(req, res, next) {});
 
 // error handlers
 
-/*
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     console.log('error ' + (err.status || 500))
-//    res.status(err.status || 500);
-
-/!*    res.render('error', {
-      message: err.message,
-      error: err
-    });*!/
   });
 }
-
+/*
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
--- public/js/common/WSServer.js
+++ public/js/common/WSServer.js
@@ -1,7 +1,11 @@
 var server;
 var Observable = require('../common/Observable').class;
+var WebSocketServer = require('websocket').server;
 
-function WSServer() {}
+function WSServer(http) {
+    this.http = http;
+    this.connections = [];
+}
 
 WSServer.prototype = Object.create(Observable.prototype);
 WSServer.prototype.constructor = WSServer;
@@ -11,16 +15,62 @@ WSServer.prototype.addListener = function(property, listener) {
     this.listeners[property] = this.listeners[property] || [];
     if (listener != null && ['object', 'function'].indexOf(typeof(listener)) > -1) {
         this.listeners[property].push(function() {
-            //listener
-        });
+
+        }());
     }
 };
 
+WSServer.prototype.init = function() {
+    this.ws = new WebSocketServer({
+        httpServer: webServer,
+        // You should not use autoAcceptConnections for production
+        // applications, as it defeats all standard cross-origin protection
+        // facilities built into the protocol and the browser.  You should
+        // *always* verify the connection's origin and decide whether or not
+        // to accept it.
+        autoAcceptConnections: false
+    });
+    this._initEvents();
+};
 
+WSServer.prototype._initEvents = function() {
+    function originIsAllowed(origin) {
+        // put logic here to detect whether the specified origin is allowed.
+        return true;
+    }
+    this.ws.on('request', function (request) {
+        if (!originIsAllowed(request.origin)) {
+            // Make sure we only accept requests from an allowed origin
+            request.reject();
+            console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+            return;
+        }
 
+        var connection = request.accept('echo-protocol', request.origin);
+        console.log((new Date()) + ' Connection accepted.');
+        connection.on('message', function (message) {
+            console.info();
+            if (message.type === 'utf8') {
+                console.log('Received Message: ' + message.utf8Data);
+                connection.sendUTF(message.utf8Data);
+            }
+            else if (message.type === 'binary') {
+                console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+                connection.sendBytes(message.binaryData);
+            }
+        });
+        connection.on('close', function (reasonCode, description) {
+            console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+        });
+    });
+};
 
-server = new WSServer();
 module.exports = {
     class: WSServer,
-    instance: server
+    instance: function(http) {
+        if (!server) {
+            server = new WSServer(http).init();
+        }
+        return server;
+    }
 };
