4e9741cccecdece09c32a53fda66a06592d8d0f7
gingerP <new.vinni@gmail.com>
2016-09-02 21:20:47 +0300

    - refactoring

--- src/js/service/ConstructorService.js
+++ src/js/service/ConstructorService.js
@@ -1,50 +1,62 @@
-var utils = _req('src/js/utils');
-var log = _req('src/js/logger');
-var logger = {
-  tbc: log.create('ConstructorService')
-};
-var c = require('../constants');
-var configDBManager= require('../db/ParseConfigDBManager').instance;
-var GenericService = require('./GenericService').class;
-var service;
+(function () {
+    'use strict';
 
-ConstructorService = function() {};
-ConstructorService.prototype = Object.create(GenericService.prototype);
-ConstructorService.prototype.constructor = ConstructorService;
-ConstructorService.prototype.test = function(id, callback) {
-    var inst = this;
-    this.manager.get(id).then(function(config) {
-        inst.testByConfig(config, callback);
-    })
-};
-ConstructorService.prototype.testByConfig = function(config, callback) {
-    var url = config.url;
-    logger.tbc.info('START test loading and parsing for config "%s(%s)"', config.code, config._id);
-    utils.loadDom(url, function(error, body) {
-        var data;
-        if (typeof(callback) == 'function') {
-            if (error) {
-                logger.tbc.info('FINISH test loading and parsing for config "%s(%s)" with error: %s' , config.code, config._id, error.message);
-                callback(error);
-                return;
-            }
-            try {
-                data = utils.extractDataFromHtml(body, config);
-            } catch (error) {
-                logger.tbc.info('FINISH test loading and parsing for config "%s(%s)" with error: %s', config.code, config._id, error.message);
-                callback(error);
-                return;
-            }
-            logger.tbc.info('FINISH test loading and parsing for config "%s(%s)"', config.code, config._id);
-            callback(data);
-        }
-    }, 'koi8r');
-};
+    var httpUtils = _req('src/js/utils/http-utils');
+    var utils = _req('src/js/utils');
+    var log = _req('src/js/logger');
+    var logger = {
+        tbc: log.create('ConstructorService')
+    };
+    var constants = require('../constants');
+    var configDBManager = require('../db/ParseConfigDBManager').instance;
+    var GenericService = require('./GenericService').class;
+    var service;
 
-service = new ConstructorService();
-service.setManager(configDBManager);
+    function ConstructorService() {
+    }
 
-module.exports = {
-    class: ConstructorService,
-    instance: service
-};
+    ConstructorService.prototype = Object.create(GenericService.prototype);
+    ConstructorService.prototype.constructor = ConstructorService;
+
+    ConstructorService.prototype.test = function (id, callback) {
+        var inst = this;
+        this.manager.get(id).then(function (config) {
+            inst.testByConfig(config, callback);
+        })
+    };
+    ConstructorService.prototype.testByConfig = function (config, callback) {
+        var url = config.url;
+        logger.tbc.info('START test loading and parsing for config "%s(%s)"', config.code, config._id);
+        httpUtils
+            .loadDom(url, httpUtils.getDefaultHeaders())
+            .then(
+                function (result) {
+                    var data;
+                    if (typeof(callback) == 'function') {
+                        try {
+                            data = utils.extractDataFromHtml(result.body, config);
+                        } catch (error) {
+                            logger.tbc.info('FINISH test loading and parsing for config "%s(%s)" with error: %s', config.code, config._id, error.message);
+                            callback(error);
+                            return;
+                        }
+                        logger.tbc.info('FINISH test loading and parsing for config "%s(%s)"', config.code, config._id);
+                        callback(data);
+                    }
+                },
+                function (result) {
+                    logger.tbc.info('FINISH test loading and parsing for config "%s(%s)" with error: %s', config.code, config._id, result.error.message);
+                    callback(result.error);
+                }
+            );
+    };
+
+    service = new ConstructorService();
+    service.setManager(configDBManager);
+
+    module.exports = {
+        class: ConstructorService,
+        instance: service
+    };
+
+})();
